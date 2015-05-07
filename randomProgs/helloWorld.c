cc0fd5432a5afae19f24e30e3b358aab31d1c965
ceisenhart <ceisenhart@soe.ucsc.edu>
2015-05-07 11:22:02 -0700

    Playing around with branches and linking things

--- randomProgs/helloWorld.c
+++ randomProgs/helloWorld.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 #include "basic.h" 
 
 int main()
 {
   printf("Hello World!\n");
   printStats("Will this work?\n");
+  if (strcmp("HelloWorld","HelloWorld")) printf("sweet!");
+
 }
