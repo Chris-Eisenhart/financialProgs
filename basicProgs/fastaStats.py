dd81aef3aa219a84a7700ff74dd2d4b7bb44336b
ceisenhart <ceisenhart@soe.ucsc.edu>
2015-05-11 12:21:15 -0700

    Fixing the program up a bit, added some new output stats for contigs of varying length

--- basicProgs/fastaStats.py
+++ basicProgs/fastaStats.py
@@ -1,4 +1,4 @@
-s#!/usr/bin/env python2.7
+#!/usr/bin/env python2.7
 # fastaStats
 # Chris Eisenhart 05/11/2015 
 # ceisenha@ucsc.edu/ceisenhart@soe.ucsc.edu 
@@ -16,28 +16,6 @@ import  re, fastFunctions
 alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'"
 # The accepted characters in a word. A word is defined as any
 # collection of characters from this alphabet. 
-def parseArgs(args): 
-    """
-    INPUT:
-	args: The command line information at compile time. 
-    OUTPUT: 
-	args: A tuple with five elements. 
-    Parses the user supplied commands. The result is a tuple
-    with five elements, one for each option. The options
-    are descend, ascend, alphabet, inputFile and outputFile.  
-    """
-    parser = argparse.ArgumentParser(description = __doc__)
-    parser.add_argument ("--inputFile",
-    help = " Specifies that the text should be read in from an"
-		" input file",
-    action = "store")
-    parser.add_argument ("--outputFile",
-    help = " Specifies that the word and count pairs should"
-		" be printed to an output file",
-    action = "store")
-    options = parser.parse_args()
-    return options
-
 
 def main(args):
     """
@@ -47,12 +25,25 @@ def main(args):
     totalBases = 0
     totalSeqs = 0
     longestContig = 0
+    contigs3k = 0
+    contigs5k = 0
+    print ("")
     for fasta in fastFunctions.readFasta (inputFile, alphabet, False, True):
         totalBases += len(fasta.sequence)
         totalSeqs += 1
-        if len(fasta.sequence) > longestContig: longestContig = len(fasta.sequence)
+        if len(fasta.sequence) > 1000: contigs1k +=1 
+        if len(fasta.sequence) > 3000: contigs3k +=1 
+        if len(fasta.sequence) > 5000: contigs5k +=1 
+        if len(fasta.sequence) > 7000: contigs7k +=1 
+        if len(fasta.sequence) > 9000: contigs9k +=1 
+        if len(fasta.sequence) > longestContig:
+            longestContig = len(fasta.sequence)
     print ("There are %i sequences with an average of %i bases. The longest contig is %i bases"
                 " There are %i bases total"% (totalSeqs, totalBases/totalSeqs, longestContig, totalBases))
-
+    print ("There are %i contigs of length 1000 or greater" % (contigs1k))
+    print ("There are %i contigs of length 3000 or greater" % (contigs3k))
+    print ("There are %i contigs of length 5000 or greater" % (contigs5k))
+    print ("There are %i contigs of length 7000 or greater" % (contigs7k))
+    print ("There are %i contigs of length 9000 or greater" % (contigs9k))
 if __name__ == "__main__" :
     sys.exit(main(sys.argv))
