5150605b835ab20a806cba05ee33107df90c687e
ceisenhart <ceisenhart@soe.ucsc.edu>
2015-05-29 11:40:37 -0700

    The program is working, commments will be up soon

--- basicProgs/stash.sh
+++ basicProgs/stash.sh
@@ -10,11 +10,16 @@ Stashes the current file with a date and time stamp in a hidden folder .stash.
 Currently .stash must already exist, comments are not supported but may be 
 coming soon ;) " 1>&2
 }
-
 # Spit usage when no arguments are given
 if [ $# -lt 1 ]; then
     usage 
     exit 255
+fi
+
+# Check if there is a stash, make one if there is not
+export DIRECTORY=".stash"
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir "$DIRECTORY" 
 fi
 
 # Check that the file given is a valid file, spit usage otherwise
@@ -24,4 +29,16 @@ if [ ! -s "${fileToStash}" ]; then
     usage
 fi
 
-
+cd .stash
+echo ${fileToStash} > aRandomFileName1
+date >> aRandomFileName1 
+cat aRandomFileName1 | tr -d ' \t\n\r\f' > aRandomFileName2
+echo  >> aRandomFileName2
+file="aRandomFileName2"
+while IFS= read -r line
+do 
+    cp "${fileToStash}" "$line"
+done < "$file" 
+rm aRandomFileName1
+rm aRandomFileName2
+echo "The file ${fileToStash} was succesfully stashed" 
